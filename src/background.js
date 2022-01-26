894347ac8bd795a8e41d1ea120cbf6c7c4d08781
Jan Redmann <janredmann1@gmail.com>
2022-01-26 17:58:39 +0100

    x

--- src/background.js
+++ src/background.js
@@ -1,18 +1,18 @@
 class Background {
 
-    preload (){
-        this.backgroundImage = loadImage('/assets/6707990.jpg')
+    preload() {
+        this.backgroundImage = loadImage('assets/6707990.jpg')
     }
-   draw (){
-       image(this.backgroundImage,0,0, 800, 530)
+    draw() {
+        image(this.backgroundImage, 0, 0, 800, 530)
 
-   }
+    }
 
-   
-   
 
-   displayControls (){
-    text('Arrow Keys to move. Space-bar to Boost',400,500)
 
-}
+
+    displayControls() {
+        text('Arrow Keys to move. Space-bar to Boost', 400, 500)
+
+    }
 }
